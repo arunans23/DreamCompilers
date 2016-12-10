b1f0c1c4ed406d0b2446da4c047b6c1d39ec360b
arunans23 <arunans.14@cse.mrt.ac.lk>
2016-12-10 18:18:20 +0530

    improve DiseaseFragment class

--- app/src/main/java/com/example/arunan/dreamcompilers/DiseaseFragment.java
+++ app/src/main/java/com/example/arunan/dreamcompilers/DiseaseFragment.java
@@ -2,11 +2,15 @@ package com.example.arunan.dreamcompilers;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Date;
 
 /**
  * Created by arunan on 12/9/16.
@@ -35,9 +39,55 @@ public class DiseaseFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_disease, container, false);
 
+
+
         //Title field
         mTitleField = (TextView)v.findViewById(R.id.disease_title);
         mTitleField.setText(mDisease.getTitle());
+
+
+
+        //Symptoms Field
+        mSymptomsField = (EditText)v.findViewById(R.id.disease_symptoms);
+        mSymptomsField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mDisease.setSymptoms(s.toString());
+                mDisease.setLastEditDate(new Date());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
+        //Description field
+        mDescriptionField = (EditText)v.findViewById(R.id.disease_description);
+        mDescriptionField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mDisease.setDescription(s.toString());
+                mDisease.setLastEditDate(new Date());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
 
 
