6b92872ef30311dc397056756e59423545cc9c14
arunans23 <arunans.14@cse.mrt.ac.lk>
2016-12-20 00:53:50 +0530

    clear code

--- app/src/main/java/com/example/arunan/dreamcompilers/activities/LoginActivity.java
+++ app/src/main/java/com/example/arunan/dreamcompilers/activities/LoginActivity.java
@@ -3,27 +3,18 @@ package com.example.arunan.dreamcompilers.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.Request.Method;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.example.arunan.dreamcompilers.R;
-import com.example.arunan.dreamcompilers.app.AppConfig;
 import com.example.arunan.dreamcompilers.data.AndroidDatabaseManager;
 import com.example.arunan.dreamcompilers.models.UserInfo;
 import com.example.arunan.dreamcompilers.models.UserLab;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by arunan on 12/13/16.
@@ -109,86 +100,8 @@ public class LoginActivity extends Activity{
     private void checkOnlineLogin(final String username, final String password){
         String tag_string_req = "req_login";
 
-        StringRequest strReq = new StringRequest(Method.POST,
-                AppConfig.sURLlogin, new Response.Listener<String>() {
-
-            @Override
-            public void onResponse(String response) {
-                Log.d(TAG, "Login Response: " + response.toString());
-
-                try {
-                    JSONObject jObj = new JSONObject(response);
-                    boolean error = jObj.getBoolean("error");
-
-                    // Check for error node in json
-                    if (!error) {
-
-                        String email = jObj.getString("email");
-
-                        JSONObject user = jObj.getJSONObject("user");
-                        String fullname = user.getString("fullname");
-                        String userID = user.getString("userID");
-                        String password = user.getString("password");
-                        String location = user.getString("location");
-                        long date = user.getLong("date");
-                        String roleID = user.getString("roleID");
-
-//                        UserInfo userInfo = new UserInfo(UUID.fromString(userID));
-//                        userInfo.setFullName(fullname);
-//                        userInfo.setEmail(email);
-//                        userInfo.setPassword(password);
-//                        userInfo.setRoleId(roleID);
-//
-//                        // Inserting row in users table
-//                        mUserLab.addUser(userInfo);
-
-                        JSONObject diseases = jObj.getJSONObject("diseases");
 
 
-                        // Launch main activity
-                        /*
-                        Intent intent = new Intent(LoginActivity.this,
-                                MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                        */
-                    } else {
-                        // Error in login. Get the error message
-                        String errorMsg = jObj.getString("error_msg");
-                        Toast.makeText(getApplicationContext(),
-                                errorMsg, Toast.LENGTH_LONG).show();
-                    }
-                } catch (JSONException e) {
-                    // JSON error
-                    e.printStackTrace();
-                    Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
-                }
-
-            }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "Login Error: " + error.getMessage());
-                Toast.makeText(getApplicationContext(),
-                        error.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        }) {
-
-            @Override
-            protected Map<String, String> getParams() {
-                // Posting parameters to login url
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("email", username);
-                params.put("password", password);
-
-                return params;
-            }
-
-        };
-
-        // Adding request to request queue
-        //AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
     }
 
     private void processResponse(String response){
